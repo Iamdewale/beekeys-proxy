1005c6780ea0b3bbf4edb86b20d7ad6d3106a303
Iamdewale <dewalefams@gmail.com>
2025-08-21 18:42:39 +0100

    markers view point update

--- routes/state.js
+++ routes/state.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getStateDetails } = require("../controllers/stateController");
+const { getStateDetails, getMarkersInViewport } = require("../controllers/stateController");
 
+
+// Route to get state details by slug
+router.get("/markers-in-viewport", getMarkersInViewport);
 router.get("/:slug", getStateDetails);
 
 module.exports = router;
